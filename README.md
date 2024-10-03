c684486e4fc72e446916d71ce99954f505e1297c
Joe D <joedef@gmail.com>
2024-10-03 15:41:52 -0400

    incr dev: dialog box skeleton

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+> **THIS IS A WORK IN PROGRESS.  BE PATIENT.**
+
 # luks-tray
 System tray applet to help mount/unmount LUKS containers (on Linux)
 
@@ -11,6 +13,7 @@ System tray applet to help mount/unmount LUKS containers (on Linux)
   - Mounted and Unlocked but trying to dismount: use   ⚠️ (U+26A0)  + 🔓 (U+1F513).
   - Unmounted and Unlocked: use 🔓 (U+1F513).
   - Uncertain: use ❓ (U+2753) 
+  - OR Open Book (📖 U+1F4D6) and Closed Book (📕 U+1F4D5) and Trophy (🏆 U+1F3C6) for mounted
 - each time the menu is opened, the state is re-establish;  and that is done periodically in the background
 - a json file will be updated when anything persistent is changed.   it will have entries with:
   - UUID
@@ -25,14 +28,39 @@ System tray applet to help mount/unmount LUKS containers (on Linux)
   - delay minutes for auto-unmount
   - repeat minutes for auto-unmount
   - OK + CANCEL buttons
-- there are persistent options in a .ini file:
+- there are persistent options in a .ini file set from the menu:
   - "mask password" (True or False) ... a menu item toggles the value
   - "retain passwords in session" another menu item persistent option (that is only shown if no master password).
+- there are .ini file options:
+  - for "unlocked and unmounted" a repeat interval in minutes to automatically unlock; if 0, then n/a
+- in the case that the app opens and finds an opened container mounted at (/,/home,/var,/var/log,/swap,/tmp,/svr,/opt,/usr) by default, then the container will be "unlisted" [to cover simple and complex full disk encryption cases where you don't want to alert "exposeed" data, etc, because is always present].   This list will be in the .ini file.
+- for handling files, there will be a section in the list for "registered" file containers and a menu item to add one. If registered and not present, it is not shown but not unregistered. File handling requires that the distro shows mounted file containers as "loop" devices by lsblk.
+- the "mount container" dialog will have a "Hide" button in addition to OK and cancel.  The hide will unregister a file container and remove it from the "Registered" section of .ini file and for partitions, will put the UUID in a "Hidden" section.  To take a partition out of the "Hidden" section, then you have to edit the .ini and know the UUID.  If opened, unregistered and hidden items are shown in the menu, affect the icon state, and can be dismounted/closed.
+- Edge cases (with caveats in the README):
+  - no filesystems in a LUKS container
+  - multiple filesystem in a LUKS container ... pretend there is just one
+  - have a lazy, soft whether open state if no child filesystems so ... it checked before an unlock/mount dialog
 - "master password" for the app that used to encrypt a password/json file with uuid and known passwords.
   - It would be optional ... so click a menu item that says "set master password" (if no master password) or
   - "change master password" that allows changing it ... each with the appropriate dialog.
-- ICON:  a shield with four variations:
-  - yellow - all mounted (info exposed)
-  - orange - some mounted (some info exposed)
-  - green - none mounted (not info exposed)
-  - white - no LUKS partitions present
+- ICON:  a shield with these variations:
+  - yellow with with big red exclamation inside - some are in dismounting in progress or unlocked but unmounted
+  - yellow - some mounted (info exposed) w/o any being dismounted or unlocked but unmounted
+  - green - none mounted (no info exposed)
+
+---
+Test Notes:
+  - for no filesystems:
+      sudo dd if=/dev/zero of=/tmp/test_luks_container bs=1M count=100
+      sudo cryptsetup luksFormat /tmp/test_luks_container
+      sudo cryptsetup open /tmp/test_luks_container test_luks
+  - for 2 file systems:
+      sudo pvcreate /dev/mapper/test_luks
+      sudo vgcreate test_vg /dev/mapper/test_luks
+      sudo lvcreate -L 20M -n lv1 test_vg
+      sudo lvcreate -L 20M -n lv2 test_vg
+      sudo mkfs.ext4 /dev/test_vg/lv1
+      sudo mkfs.ext4 /dev/test_vg/lv2
+
+"Full disk encryption" is usually just / (but might also include /home, /var, /var/log, /swap/, /tmp/, /svr, /opt, /usr, /var/log)
+
--- src/luks_tray/main.py
+++ src/luks_tray/main.py
@@ -2,17 +2,131 @@ import os
 import sys
 import json
 import signal
+import subprocess
+import traceback
 from types import SimpleNamespace
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QMessageBox, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton
-from PyQt5.QtGui import QIcon, QCursor
-from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon, QCursor, QFont
+from PyQt5.QtCore import QTimer, Qt
 
 import luks_tray.Utils as Utils
 from luks_tray.Utils import prt, PyKill
 from luks_tray.IniTool import IniTool
 
 
-class LUKSTrayApp():
+class DeviceInfo:
+    """ Class to dig out the info we want from the system."""
+    def __init__(self, opts):
+        self.opts = opts
+        self.DB = opts.debug
+        self.wids = None
+        self.head_str = None
+        self.partitions = None
+        self.entries = {}
+
+    @staticmethod
+    def _make_partition_namespace(name, size_bytes):
+        return SimpleNamespace(name=name,       # /proc/partitions
+            opened=None,    # or True or False
+            label='',       # blkid
+            fstype='',      # fstype OR /sys/class/block/{name}/device/model
+            size_bytes=size_bytes,  # /sys/block/{name}/...
+            uuid='',
+            mounts=[],      # /proc/mounts
+            parent=None,    # a partition
+            filesystems=[],        # child file systems
+            )
+
+
+    @staticmethod
+    def get_device_vendor_model(device_name):
+        """ Gets the vendor and model for a given device from the /sys/class/block directory.
+        - Args: - device_name: The device name, such as 'sda', 'sdb', etc.
+-       - Returns: A string containing the vendor and model information.
+        """
+        def get_str(device_name, suffix):
+            try:
+                rv = ''
+                fullpath = f'/sys/class/block/{device_name}/device/{suffix}'
+                with open(fullpath, 'r', encoding='utf-8') as f: # Read information
+                    rv = f.read().strip()
+            except (FileNotFoundError, Exception):
+                # print(f"Error reading {info} for {device_name} : {e}")
+                pass
+            return rv
+
+        # rv = f'{get_str(device_name, "vendor")}' #vendor seems useless/confusing
+        rv = f'{get_str(device_name, "model")}'
+        return rv.strip()
+
+    def parse_lsblk(self):
+        """ Parse ls_blk for all the goodies we need """
+        def eat_one(device):
+            entry = self._make_partition_namespace('', '')
+            entry.name=device.get('name', '')
+            entry.fstype = device.get('fstype', '')
+            if entry.fstype is None:
+                entry.fstype = ''
+            entry.label = device.get('label', '')
+            if not entry.label:
+                entry.label=device.get('partlabel', '')
+            if entry.label is None:
+                entry.label = ''
+            entry.size_bytes=int(device.get('size', 0))
+            entry.uuid = device.get('uuid', '')
+            mounts = device.get('mountpoints', [])
+            while len(mounts) >= 1 and mounts[0] is None:
+                del mounts[0]
+            entry.mounts = mounts
+
+            return entry
+
+               # Run the `lsblk` command and get its output in JSON format with additional columns
+        result = subprocess.run(['lsblk', '-J', '--bytes', '-o',
+                    'NAME,MAJ:MIN,FSTYPE,LABEL,PARTLABEL,FSUSE%,SIZE,UUID,MOUNTPOINTS', ],
+                    stdout=subprocess.PIPE, text=True, check=False)
+        parsed_data = json.loads(result.stdout)
+        entries = {}
+
+        # Parse each block device and its properties
+        for device in parsed_data['blockdevices']:
+            parent = eat_one(device)
+            parent.fstype = self.get_device_vendor_model(parent.name)
+            for child in device.get('children', []):
+                entry = eat_one(child)
+                # entry.parent = parent.name
+                entry.parent = parent
+                if not parent.fstype:
+                    parent.fstype = 'DISK'
+                elif 'luks' not in entry.fstype.lower():
+                    continue
+                # if parent.name not in entries:
+                    # entries[parent.name] = parent
+                entries[entry.uuid] = entry
+                grandchildren = child.get('children', None)
+                if not isinstance(grandchildren, list):
+                    entry.opened = False
+                    continue
+                entry.opened = True
+                for grandchild in child.get('children'):
+                    subentry = eat_one(grandchild)
+                    subentry.parent = entry.name
+                    entry.filesystems.append(subentry)
+                    # entries[subentry.name] = subentry
+
+        self.entries = entries
+        if self.DB:
+            print('\n\nDB: --->>> after parse_lsblk:')
+            for entry in entries.values():
+                print(vars(entry))
+
+        return entries
+
+    def get_relative(self, name):
+        return self.entries.get(name, None)
+
+class LuksTray():
+    singleton = None
     svg_info = SimpleNamespace(version='03', subdir='resources/SetD'
                 , bases= [
                         'white-shield',  # no LUKS partitions 
@@ -21,11 +135,11 @@ class LUKSTrayApp():
                         'yellow-shield',  # no partitions locked (all locked)
                         ] )
 
-    def __init__(self, ini_tool):
+    def __init__(self, ini_tool, opts):
+        LuksTray.singleton = self
         self.ini_tool = ini_tool
         self.app = QApplication([])
         self.app.setQuitOnLastWindowClosed(False)
- 
 
         self.icons, self.svgs = [], []
         for base in self.svg_info.bases:
@@ -40,7 +154,8 @@ class LUKSTrayApp():
 
         # Load JSON data
         self.load_data()
-
+        self.lsblk = DeviceInfo(opts=opts)
+        self.containers = self.lsblk.parse_lsblk()
 
         self.tray_icon = QSystemTrayIcon(self.icons[0], self.app)
         self.tray_icon.setToolTip('luks-tray')
@@ -60,29 +175,70 @@ class LUKSTrayApp():
                 self.data = json.load(f)
         except FileNotFoundError:
             self.data = {}
-
+        
+    def show_partition_details(self, name):
+        container = self.containers.get(name, None)
+        if container is None:
+            return
+        details = f'DETAILS for {container.name}:\n'
+        details += 'UUID={container.UUID}\n'
+        QMessageBox.information(None, "Partition Details", details)
+        
     def update_menu(self):
         """Update context menu with LUKS partitions."""
         self.menu.clear()
+        mono_font = QFont("Consolas", 10)
         
         # Adding dummy partition states for demonstration
-        for uuid, details in self.data.items():
-            action = QAction(f"{details['device_name']} - {details['mount_point']} - {details['state']}", self)
-            action.triggered.connect(lambda checked, uuid=uuid: self.handle_partition_click(uuid))
+        for container in self.containers.values():
+            mountpoint = None
+            if container.opened:
+                if len(container.filesystems) >= 1:
+                    mounts = container.filesystems[0].mounts
+                    if mounts:
+                        mountpoint = mounts[0]
+
+            # Set the title based on the state
+            title = '⯅' if mountpoint else '❢ '
+            title += '🗹' if container.opened else '—'
+            title += f' {container.name} {mountpoint}'
+
+            # Create the action for the partition
+            action = QAction(title, self.app)
+            action.setFont(mono_font)
+            
+            # Connect the left-click action
+            action.triggered.connect(lambda checked, x=container.uuid: self.handle_partition_click(x))
+
+            # Use a custom event filter to detect right-click for showing details
+            # action.customContextMenuRequested.connect(lambda: self.show_partition_details(container.name))
+
+            # Add action to the menu
             self.menu.addAction(action)
-        
-#       self.menu.addSeparator()
+
+        self.menu.addSeparator()
 
         # Add options to exit
         exit_action = QAction("Exit", self.app)
+        exit_action.setFont(mono_font)
         exit_action.triggered.connect(self.exit_app)
         self.menu.addAction(exit_action)
+
+    def show_partition_details(self, name):
+        container = self.containers.get(name, None)
+        if container is None:
+            return
+        details = f'DETAILS for {container.name}:\n'
+        details += f'UUID={container.uuid}\n'
+        QMessageBox.information(None, "Partition Details", details)
+
 
     def handle_partition_click(self, uuid):
         """Handle clicking a partition."""
         # Show a dialog to unmount or display info
-        dialog = UnmountDialog(uuid, self.data[uuid])
-        dialog.exec_()
+        if uuid in self.containers:
+            dialog = UnmountDialog(self.containers[uuid])
+            dialog.exec_()
 
     def exit_app(self):
         """Exit the application."""
@@ -90,14 +246,17 @@ class LUKSTrayApp():
         sys.exit()
 
 class UnmountDialog(QDialog):
-    def __init__(self, uuid, details):
+    def __init__(self, container):
         super().__init__()
         self.setWindowTitle("Unmount Partition")
         self.setFixedSize(300, 200)
 
         layout = QVBoxLayout()
+        mounts = []
+        if container.filesystems:
+            mounts = container.filesystems[0].mounts
 
-        self.label = QLabel(f"Unmount {details['mount_point']}?")
+        self.label = QLabel(f"Unmount {mounts}?")
         layout.addWidget(self.label)
 
         self.password_input = QLineEdit()
@@ -134,14 +293,62 @@ class UnmountDialog(QDialog):
         
         self.accept()
 
+def rerun_module_as_root(module_name):
+    """ rerun using the module name """
+    if os.geteuid() != 0: # Re-run the script with sudo
+        os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        vp = ['sudo', sys.executable, '-m', module_name] + sys.argv[1:]
+        os.execvp('sudo', vp)
+
 def main():
+    import argparse
     signal.signal(signal.SIGINT, signal.SIG_DFL)
- 
-    ini_tool = IniTool(paths_only=False)
-    Utils.prt_path = ini_tool.log_path
- 
-    tray = LUKSTrayApp(ini_tool)
-    sys.exit(tray.app.exec_())
+    # os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-D', '--debug', action='store_true',
+            help='override debug_mode from .ini initially')
+    parser.add_argument('-o', '--stdout', action='store_true',
+            help='log to stdout (if a tty)')
+    parser.add_argument('-f', '--follow-log', action='store_true',
+            help='exec tail -n50 -F on log file')
+    parser.add_argument('-e', '--edit-config', action='store_true',
+            help='exec ${EDITOR:-vim} on config.ini file')
+    parser.add_argument('-q', '--quick', action='store_true',
+            help='quick mode (1m lock + 1m sleep')
+    opts = parser.parse_args()
+
+    if opts.edit_config:
+        ini_tool = IniTool(paths_only=True)
+        editor = os.getenv('EDITOR', 'vim')
+        args = [editor, ini_tool.ini_path]
+        print(f'RUNNING: {args}')
+        os.execvp(editor, args)
+        sys.exit(1) # just in case ;-)
+
+    if opts.follow_log:
+        ini_tool = IniTool(paths_only=True)
+        args = ['tail', '-n50', '-F', ini_tool.log_path]
+        print(f'RUNNING: {args}')
+        os.execvp('tail', args)
+        sys.exit(1) # just in case ;-)
+
+    try:
+        if os.geteuid() != 0:
+            # Re-run the script with sudo needed and opted
+            rerun_module_as_root('luks_tray.main')
+
+        ini_tool = IniTool(paths_only=False)
+        Utils.prt_path = ini_tool.log_path
+     
+        tray = LuksTray(ini_tool, opts)
+        sys.exit(tray.app.exec_())
+
+    except Exception as exce:
+        print("exception:", str(exce))
+        print(traceback.format_exc())
+        sys.exit(15)
+
 
 # Basic PyQt5 Tray Icon example
 def mainBasic():
